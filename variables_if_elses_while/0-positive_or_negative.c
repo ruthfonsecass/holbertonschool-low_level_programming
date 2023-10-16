37e2007b47b8d7a438c9ee121b7ba2c70c5cbb36
root <git@github.com:ruthfonsecass/MySuperRepo.git>
2023-10-16 03:13:22 -0700

    if elses

---
+++ variables_if_elses_while/0-positive_or_negative.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+int main(void)
+{
+	int n = 0;
+	while (n>0)
+		
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n<0);
+	printf("is positive\n");
+
+	else
+	printf("is zero\n")
+
+	else if (n>0) 
+	ptintf("is negative\n")
+	
+	return (0);
+}
