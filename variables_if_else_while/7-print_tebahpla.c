8cffc0f7c8af1f0c744218f2b7ffc6c4e9e39d66
root <git@github.com:ruthfonsecass/MySuperRepo.git>
2023-10-17 05:31:26 -0700

    mirror

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'print whether the number is positive or negative.'
+ * Return: Alway 0 (Success)
+ */
+int main(void)
+{
+	char alpha = 'z'
+	
+	while (alpha >= 'a')
+	{
+		putchar ('alpha');
+		alpha--
+	}
+	putchar('\n');
+	return (0);
+}
