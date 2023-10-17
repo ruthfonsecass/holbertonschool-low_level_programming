ccaccd8fc04fd4469be1d5917dc1df0a162eb9fa
root <git@github.com:ruthfonsecass/MySuperRepo.git>
2023-10-17 00:45:31 -0700

    positive or negative

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -17,12 +17,14 @@ int main(void)
 	if (n > 0)
 	{
 		printf("%d is positive", n);
-	}
-	else if (n == 0)
+	};
+
+	if (n == 0)
 	{
 		printf("%d is zero", n);
-	}
-	else
+	};
+
+	if (n < 0)
 	{
 		printf("%d is negative", n);
 	};
