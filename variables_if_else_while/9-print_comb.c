22c0ac7845bb4b97909542c17a896f124ea2de9f
root <git@github.com:ruthfonsecass/MySuperRepo.git>
2023-10-17 06:57:08 -0700

    number

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'print number.'
+ * Return: Alway 0 (Success)
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+	{
+		putchar('0' + number);
+		if (number < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+	}
+
+	puthcar('\n')
+	return (0);
+}
