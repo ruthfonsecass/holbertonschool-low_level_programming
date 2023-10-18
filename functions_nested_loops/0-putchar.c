e5be41b7766f8b5486f03301cc5ea75a56d985c2
root <git@github.com:ruthfonsecass/MySuperRepo.git>
2023-10-18 02:26:11 -0700

    _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'print _putchar.'
+ * Return: Alway 0 (Success)
+ */
+int main(void)
+{
+	printf("\_putchar\n";)
+
+	return (0);
+
+}
