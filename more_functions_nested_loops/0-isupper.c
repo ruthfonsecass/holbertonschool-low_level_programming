cd6ffb273720faa7933e1025ad51f2305858eb4b
root <git@github.com:ruthfonsecass/MySuperRepo.git>
2023-10-20 06:19:14 -0700

    isupper

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - check the code.
+ *
+ * Return: 0
+*/
+int _isupper(int c)
+{
+	if ( c >= 'A' && c <= 'Z')
+	{	
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
