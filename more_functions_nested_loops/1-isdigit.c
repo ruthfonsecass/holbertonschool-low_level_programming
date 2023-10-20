6a355ec2a90407c18973f017c1cf031adb714072
root <git@github.com:ruthfonsecass/MySuperRepo.git>
2023-10-20 06:56:02 -0700

    isdigit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for uppercase character
+ * @c: character to be checked
+ * Returns: 1 if c is a digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	for (c = 0; c <= 0; c++)
+	{
+		if (c >= 0 && c <= 9)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+}
+
