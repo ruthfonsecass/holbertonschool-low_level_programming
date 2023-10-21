cf1b7af1ea4f58dbc8f27195e88a556da11d72c5
root <git@github.com:ruthfonsecass/MySuperRepo.git>
2023-10-21 06:31:24 -0700

    fizz buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -11,23 +11,23 @@ int main(void)
 
 	while (num < 100)
 	{
-		num++
-			if ((num % 3) != 0 && (num % 5) != 0)
-			{
-				printf("%d", num);
-			}
-			else if ((num % 3) == 0 && (num % 5) != 0)
-			{
-				printf("Fizz");
-			}
-			else if ((num % 3) != 0 && (num % 5) == 0)
-			{
-				printf("Buzz");
-			}
-			else
-			{
-				printf("FizzBuzz");
-			}
+		num++;
+		if ((num % 3) != 0 && (num % 5) != 0)
+		{
+			printf("%d", num);
+		}
+		else if ((num % 3) == 0 && (num % 5) != 0)
+		{
+			printf("Fizz");
+		}
+		else if ((num % 3) != 0 && (num % 5) == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("FizzBuzz");
+		}
 		if (num < 100)
 			putchar(32);
 	}
