e8522347aef1305aaca376fce7d79f8382534c54
root <git@github.com:ruthfonsecass/MySuperRepo.git>
2023-10-21 02:05:52 -0700

    print numbers

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - check the code
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i, r;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (r = 0; r <= 14; r++)
+		{
+			{
+				if (i > 10)
+					_putchar(r / 10 + '0');
+				_putchar(r % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
