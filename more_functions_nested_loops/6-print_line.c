28c72013b5d1735baccefc34a1484b04fc0e233b
root <git@github.com:ruthfonsecass/MySuperRepo.git>
2023-10-21 03:24:26 -0700

    print line

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,6 +1,22 @@
 #include "main.h"
 
+/**
+ * print_line - check the code
+ * @n: number of times
+ * Return: void
+ */
 void print_line(int n)
 {
+	int i;
 
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
 }
