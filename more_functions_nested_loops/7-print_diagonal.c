6df9e5fe7d41d2e244c05ec18090558df9a72a72
root <git@github.com:ruthfonsecass/MySuperRepo.git>
2023-10-21 05:20:15 -0700

    diagonal

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - check the code
+ * @n: is the number of times the character
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int r, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (r = 0; r < n; r++)
+	{ 
+		for (j = 0; j < r; j++)
+			_putchar(' ');
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
