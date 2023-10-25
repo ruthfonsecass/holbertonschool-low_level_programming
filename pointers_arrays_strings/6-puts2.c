23567e3f35610dea396ec07cef18ba2587e37fec
root <git@github.com:ruthfonsecass/MySuperRepo.git>
2023-10-25 05:41:56 -0700

    puts

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -2,14 +2,14 @@
 
 /**
  * puts2 - check the code
- *
+ * @str: valeur
  * Return: void
  */
 void puts2(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	for (i = str; i <= '\0'; i++)
 	{
 		_putchar(str[i]);
 	}
