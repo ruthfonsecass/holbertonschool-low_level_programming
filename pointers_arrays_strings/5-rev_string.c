60d38de86eccf36f7ebf2e210b0b0bb9bad13e52
root <git@github.com:ruthfonsecass/MySuperRepo.git>
2023-10-25 09:22:45 -0700

    rev

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -17,9 +17,9 @@ void rev_string(char *s)
 
 	for (i = len - 1; i >= 0; i--)
 	{
-		temp = s[i];
-		s[i] = s[len - 1];
-		s[len - 1] = temp;
+		tmp = *(s + index);
+		*(s + index) = *(s + len - 1);
+		*(s + len - 1) = tmp;
 		len--;
 	}
 }
