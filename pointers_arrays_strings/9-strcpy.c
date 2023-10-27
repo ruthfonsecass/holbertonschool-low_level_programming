252cc0201fbc07f68f1da8df743ca96e80d9f2cb
root <git@github.com:ruthfonsecass/MySuperRepo.git>
2023-10-27 03:43:15 -0700

    strcpy

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strcpy - check the code
+ * @dest: valeur
+ * @src: valeur
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (dest);
+}
