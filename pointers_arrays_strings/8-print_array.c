529e6d1d49ff5ab465c27927047a51c0100e4368
root <git@github.com:ruthfonsecass/MySuperRepo.git>
2023-10-27 01:57:40 -0700

    print array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_array - check the code for
+ * @a: valeur
+ * @n: valeur
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i, valeur[a] = n;
+
+	for (i = 0; i <= n; i++)
+		printf("%d", valeur[i]);
+}
