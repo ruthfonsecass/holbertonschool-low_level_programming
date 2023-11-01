8ed540f86b4823c6cff4c5d74c590b01449976a6
root <git@github.com:ruthfonsecass/MySuperRepo.git>
2023-11-01 10:55:50 -0700

    factorial

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,12 +1,17 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
+ * factorial - check the code
+ * @n: string
  * Return: Always 0.
  */
 int factorial(int n)
 {
 	if (n < 0)
-		return (-1)
+		return (-1);
+
+	else (n <= 1)
+		return (1);
+
+	return (n * factorial(n -1);
 }
