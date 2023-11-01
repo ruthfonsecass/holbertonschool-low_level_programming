5eb7cb953b2e49265d4f914caa6faad3f152cb40
root <git@github.com:ruthfonsecass/MySuperRepo.git>
2023-11-01 07:01:35 -0700

    print

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - check the code
+ * @s: ponteur
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	_putchar(*(s));
+	if (*(s + 1))
+	{
+		s++;
+		_puts_recursion(s);
+		return;
+	}
+	_putchar('\n');
+}
---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void _print_rev_recursion(char *s)
+{
+	if(*s == '\0')
+		return;
+	
+	_print_rev_recursion(s + 1);
+
+	_putchar(*s);
+}
