bbfd95d4d102379467f9161636592d0f418b9583
root <git@github.com:ruthfonsecass/MySuperRepo.git>
2023-11-02 03:55:41 -0700

    pow

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - check the code
+ * @x: valeur
+ * @y: valeur
+ * Return: Always 0.
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (- 1);
+
+	else (y == 0)
+		return (1);
+
+	return (x * _pow_recursion(n, y - 1));
+}
