e5d65d087b328601d4c15f3b2b2f5926e6700bf4
root <git@github.com:ruthfonsecass/MySuperRepo.git>
2023-11-03 14:36:10 -0700

    recursion

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
 /**
- * _puts_recursion - check the code
- * @s: ponteur
- * Return: void
+ * _puts_recursion - Prints a string, followed by a new line.
+ * @s: The string to be printed.
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s)
 	{
-		_putchar('\n');
-		return;
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
-
-	_putchar(*s);
-	_puts_recursion(s + 1);
+	else
+		_putchar('\n');
 }
