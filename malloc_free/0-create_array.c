d7e9e8d6681984b46989facbec776325126c4de4
root <git@github.com:ruthfonsecass/MySuperRepo.git>
2023-11-08 12:07:18 -0800

    array

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * create_array - prints buffer in hexa
+ * @c:character
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	int i = 0;
+
+	arr = malloc(sizeof(char) * size);
+
+	if (size == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++;
+			arr[i] = c;
+
+			return(arr);
+			}
