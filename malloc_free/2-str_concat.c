7d76d1a27d0217140d1d3fdb0121b32ffdfd1482
root <git@github.com:ruthfonsecass/MySuperRepo.git>
2023-11-09 06:34:05 -0800

    concat

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,42 +1,50 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * str_concat - check the code
- * @s1: string
+ * str_concat - Concatenates two strings
+ * @s1: string 1
  * @s2: string 2
- * Return: Always 0.
+ * Return: returns the concatenated string
  */
+
 char *str_concat(char *s1, char *s2)
 {
-	char *ptr;
-	int len1 = 0, len2 = 0, i = 0;
+    int i = 0;
+    char *strced;
+    int len1 = 0;
+    int len2 = 0;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s1 = "";
-	while (s1[len1] != '\0')
-	{
-		len1++;
-	}
-	while (s2[len2] != '\0')
-	{
-		len2++;
-	}
-
-	ptr = malloc(len1 + len2 + 1);
-
-	for (i = 0; i < len1; i++)
-	{
-		ptr[i] = s1[i];
-	}
-
-	for (i = 0; i < len2; i++)
-	{
-		ptr[len1 + i] = s2[i];
-	}
-	ptr[len1 + len2] = '\0';
-	return (ptr);
+    if (s1 == NULL)
+    {
+        s1 = "";
+    }
+    if (s2 == NULL)
+    {
+        s2 = "";
+    }
+    while (s1[len1] != '\0')
+    {
+        len1++;
+    }
+    while (s2[len2] != '\0')
+    {
+        len2++;
+    }
+    strced = malloc(sizeof(char) * (len1 + len2 + 1));
+    if (strced == NULL)
+    {
+        return (NULL);
+    }
+    for (i = 0 ; i < len1 ; i++)
+    {
+        strced[i] = s1[i];
+    }
+    for (i = 0 ; i < len2 ; i++)
+    {
+    strced[len1 + i] = s2[i];
+    }
+    strced[len1 + len2] = '\0';
+    return (strced);
 }
