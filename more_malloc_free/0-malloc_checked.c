290080d3381278bc9740638d014c419a59ddb886
root <git@github.com:ruthfonsecass/MySuperRepo.git>
2023-11-10 06:30:40 -0800

    ficher

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+
 /**
  * malloc_checked - Allocates memory using malloc
  * @b: Size of the memory to allocate
  * Return: Pointer to the allocated memory, or NULL on failure
  */
-
 void *malloc_checked(unsigned int b)
 {
 	void *check;
