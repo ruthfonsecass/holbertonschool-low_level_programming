896f69c870aa07ded21236b8bbb7e662bfa20bc1
root <git@github.com:ruthfonsecass/MySuperRepo.git>
2023-11-16 06:17:13 -0800

    print name

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_name_as_is - prints a name as is
+ * @name: name of the person
+ *
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
