44ad280af740cd2ce66251395f8efefa1adf0818
root <git@github.com:ruthfonsecass/MySuperRepo.git>
2023-11-16 01:38:39 -0800

    ficher

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -7,9 +7,17 @@
  * @age: age of the dog
  * @owner: owner of the dog
 */
- struct dog{
+ struct dog
+{
 	char *name;
 	float age;
 	char *owner;
  };
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
