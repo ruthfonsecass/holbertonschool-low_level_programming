cd19a4aec47915699edfd66905ef28987ca25158
root <git@github.com:ruthfonsecass/MySuperRepo.git>
2023-11-21 04:14:46 -0800

    reverse array

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reverse_array - print an array of integers
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ * Return: nothing.
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int l = n -1;
+
+	for (i = 0; i < (n / 2); i++)
+	{
+		int temp = a[i];
+		a[i] = a[l];
+		a[l] = temp;
+	}
+}
