918f156eb5be775ef33d89697a3ddbae756d1d8d
root <git@github.com:ruthfonsecass/MySuperRepo.git>
2023-11-29 06:56:02 -0800

    update

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -8,31 +8,17 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	char *dup_str;
 	list_t *new_node;
-	size_t len = 0;
-
-	while (str[len])
-		len++;
+	int i;
 
 	new_node = malloc(sizeof(list_t));
-
 	if (new_node == NULL)
 		return (NULL);
-
-	dup_str = strdup(str);
-
-	if (dup_str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-
-	new_node->str = dup_str;
-	new_node->len = len;
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	new_node->str = strdup(str);
+	new_node->len = i;
 	new_node->next = *head;
-
 	*head = new_node;
-
 	return (new_node);
 }
