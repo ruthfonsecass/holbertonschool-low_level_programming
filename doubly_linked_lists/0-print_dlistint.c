c11241fb08851892b26a1f7a02e892c92b0db419
root <git@github.com:ruthfonsecass/MySuperRepo.git>
2023-11-30 02:35:53 -0800

    print dlistint

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - check the code
+ * @h: pointeur
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (i != '\0')
+	{
+		printf("%d\n", h->n);
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
