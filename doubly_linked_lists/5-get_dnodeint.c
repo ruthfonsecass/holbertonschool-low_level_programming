79769b2415892e217afeb3f4febc4c565620c031
root <git@github.com:ruthfonsecass/MySuperRepo.git>
2023-12-01 13:18:26 -0800

    ficher

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - returns the nth node of a dlistint_t linked list
+ * @head: pointer to the head of the list
+ * @index: index of the node to return
+ * Return: the nth node of a dlistint_t linked list, or NULL if the node
+ * does not exist
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	for (; index != 0; index--)
+	{
+		if (head == NULL)
+			return (NULL);
+		head = head->next;
+	}
+
+	return (head);
+}
